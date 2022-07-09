f43ac4be4c590df75192f87179f6f4b90cffbd67
Kevin Castillo <castilloreyeskm@gmail.com>
2022-07-09 13:30:35 -0500

    Add form controls and validations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'temperature-coverter';
+export class AppComponent implements OnInit {
+  temperatureTypes: Array<string> = [ 'Celsius', 'Farenheit'];
+  result: string = "--";
+
+  temperatureForm!: FormGroup;
+  degree!: FormControl;
+  conversionFromType!: FormControl;
+  conversionToType!: FormControl;
+
+  get isSameConversionType(): boolean {
+    return this.conversionFromType.value === this.conversionToType.value;
+  }
+  ngOnInit(): void {
+    this.initFormControls();
+    this.initForm();
+  }
+
+  initFormControls(): void {
+    this.degree = new FormControl("", [Validators.required, Validators.pattern("^[0-9]+$")]);
+    this.conversionFromType = new FormControl("", Validators.required);
+    this.conversionToType = new FormControl("", Validators.required);
+  }
+
+  initForm() :void {
+    this.temperatureForm = new FormGroup({
+      degree: this.degree,
+      conversionFromType:  this.conversionFromType,
+      conversionToType:  this.conversionToType
+    });
+  }
+
+  onSubmit() :void {
+    if(this.temperatureForm.valid && !this.isSameConversionType){
+      console.log(this.temperatureForm.value);
+      this.resetForm();
+    }
+  }
+
+  private resetForm(): void {
+    this.temperatureForm.reset();
+    this.conversionFromType.setValue(""); 
+  }
+
 }
